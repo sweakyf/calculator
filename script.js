2b8b0a5fb760e92be90d053d9ebf9bea25013c6f
sweakyf <115627830+sweakyf@users.noreply.github.com>
2022-10-12 20:35:36 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function calc(number1, operator, number2)  {
+    if (operator == '+') {
+        alert(number1 + number2)
+    }
+    if (operator == '-') {
+        alert(number1 - number2)
+    }
+    if (operator == '*') {
+        alert(number1 * number2)
+    }
+    if (operator == '/') {
+        alert(number1 / number2)
+    }
+}
+
+calc(13, '*', 23)
