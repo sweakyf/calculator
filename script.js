c3faba68f57cc59327c917964eddee8773269bb9
sweakyf <115627830+sweakyf@users.noreply.github.com>
2022-10-21 18:36:44 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
-function calc(number1, operator, number2)  {
-    if (operator == '+') {
-        alert(number1 + number2)
-    }
-    if (operator == '-') {
-        alert(number1 - number2)
-    }
-    if (operator == '*') {
-        alert(number1 * number2)
-    }
-    if (operator == '/') {
-        alert(number1 / number2)
+function calc1(number1, operator, number2)  {
+    if (operator === '/' && number2 === 0) {
+        alert('На ноль делить нельзя');
+
+    } else if (operator === '+') {
+        alert(number1 + number2);
+
+    } else if (operator === '-') {
+        alert(number1 - number2);
+
+    } else if (operator === '*') {
+        alert(number1 * number2);
+
+    } else if (operator === '/') {
+        alert(number1 / number2);
     }
 }
 
-calc(13, '*', 23)
+function calc2(number1, operator, number2) {
+    switch (operator) {
+        case '*':
+            alert(number1 *  number2);
+            break;
+        case '/':
+            number2 === 0
+                ? alert('На ноль делить нельзя')
+                : alert(number1 /  number2);
+
+            break;
+        case '+':
+            alert(number1 +  number2);
+            break;
+        case '-':
+            alert(number1 -  number2);
+            break;
+    
+        default:
+            break;
+    }
+}
+
+calc1(13, '*', 23);
+calc2(13, '*', 23);
+calc2(13, '/', 0);
+calc2(13, '/', 2);
